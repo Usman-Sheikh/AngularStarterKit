efe865b9d4d2daae6229b03a6ff6cfa985ac29fe
Usman-Sheikh <sh.usmanikram@hotmail.com>
2016-03-25 00:31:17 +0500

    Version 1

---
+++ app/Controllers/ContactController.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('eventsApp')
+        .controller('ContactController', ContactController);
+
+    ContactController.$inject = ['$scope']; 
+
+    function ContactController($scope) {
+        $scope.title = 'ContactController';
+        $scope.active = 'active';
+        $scope.Message = 'Hello From Angular';
+        activate();
+
+        function activate() { }
+    }
+})();
---
+++ app/app.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="Views/Home/Home.html" />
+/// <reference path="Views/Home/Home.html" />
+/// <reference path="Views/Home/Home.html" />
+'use strict';
+
+var eventsApp = angular.module('eventsApp', ['ngRoute'])
+
+    .config(function ($routeProvider) {
+
+        $routeProvider.when('/Home', {
+            templateUrl: 'Views/Home/Home.html',
+            controller:'HomeController'
+        })
+        .when('/Products', {
+            templateUrl: 'Views/Products/List.html',
+            controller: 'ProductsController'
+        })
+       .when('/Contact', {
+                templateUrl: 'Views/Home/Contact.html',
+                controller: 'ContactController'
+       })
+       .otherwise('/', {
+           templateUrl: 'Views/Home/Home.html',
+            controller: 'HomeController'
+       })
+
+   
+    
+    });
